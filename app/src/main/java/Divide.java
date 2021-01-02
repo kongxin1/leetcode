ecd50539ac151474575c5131c862f8ccb2196dc8
kongxin1 <kongxin1912@163.com>
2021-01-02 14:21:51 +0800

    29. 两数相除

--- app/src/main/java/Divide.java
+++ app/src/main/java/Divide.java
@@ -14,22 +14,45 @@ public class Divide {
         if(dividend==0){
             return 0;
         }
+        long ldividend=dividend,ldivisor=divisor;
         long res=0;
-        long tdivisor=divisor;
-        while(true){
-            if(dividend<0&&divisor<0&&dividend<=tdivisor){
-                res++;
-            }else if(dividend>0&&divisor>0&&dividend>=tdivisor){
-                res++;
-            }else if(dividend>0&&divisor<0&&(-dividend)<=tdivisor){
-                res--;
-            }else if(dividend<0&&divisor>0&&dividend<=(-tdivisor)){
-                res--;
-            }else{
-                break;
-            }
-            tdivisor+=divisor;
+        boolean positive=true;
+        if(ldividend<0&&ldivisor>0||ldividend>0&&divisor<0){
+            positive=false;
         }
-        return (int)res;
+        //将负数值全部转换为正数
+        if(ldividend<0){
+            ldividend=-ldividend;
+        }
+        if(divisor<0){
+            ldivisor=-ldivisor;
+        }
+        while(ldividend>=ldivisor){
+            int bit=shiftBit(ldividend,ldivisor);
+            ldividend=ldividend-(ldivisor<<bit);
+            res=res|(1L<<bit);
+            if(positive){
+                if(res>Integer.MAX_VALUE){
+                    return Integer.MAX_VALUE;
+                }
+            }else{
+                if(-res<Integer.MIN_VALUE){
+                    return Integer.MAX_VALUE;
+                }
+            }
+        }
+        if(!positive){
+            return (int)-res;
+        }else{
+            return (int)res;
+        }
+    }
+    private int shiftBit(long a,long b){
+        int bit=0;
+        while(a>=(b<<1)){
+            bit++;
+            b=b<<1;
+        }
+        return bit;
     }
 }
