09f6740b25fdc1974a98c859b41de03de27ebd40
kongxin1 <kongxin1912@163.com>
2021-02-16 13:22:04 +0800

    144. 二叉树的前序遍历

---
+++ app/src/main/java/preorderTraversal/PreorderTraversal.java
@@ -0,0 +1,47 @@
+package preorderTraversal;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class PreorderTraversal {
+    public static void main(String[] args)throws Exception {
+        PreorderTraversal code=new PreorderTraversal();
+        TreeNode right=new TreeNode(2,new TreeNode(3),null);
+        TreeNode root=new TreeNode(1,null,right);
+        System.out.println(code.preorderTraversal(root));
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res=new ArrayList<>();
+        traverByNotRecursion(root,res);
+        return res;
+    }
+    //递归遍历
+    private void traverByRecursion(TreeNode root,List<Integer> list) {
+        if(root==null){
+            return ;
+        }
+        list.add(root.val);
+        this.traverByRecursion(root.left,list);
+        this.traverByRecursion(root.right,list);
+    }
+    //非递归遍历
+    private void traverByNotRecursion(TreeNode root,List<Integer> list) {
+        if(root==null){
+            return ;
+        }
+        //使用stack模拟计算机栈
+        List<TreeNode> stack=new ArrayList<>();
+        stack.add(root);
+        do{
+            TreeNode node=stack.remove(stack.size()-1);
+            list.add(node.val);
+            if(node.right!=null){
+                stack.add(node.right);
+            }
+            if(node.left!=null){
+                stack.add(node.left);
+            }
+        }while(stack.size()!=0);
+    }
+}
