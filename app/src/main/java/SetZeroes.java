0d3fb0fbd2835b01b63804b32fe3abcbe3f6e3f2
kongxin1 <kongxin1912@163.com>
2021-07-05 21:50:40 +0800

    73. 矩阵置零

--- app/src/main/java/SetZeroes.java
+++ app/src/main/java/SetZeroes.java
@@ -1,33 +1,46 @@
 public class SetZeroes {
     public static void main(String[] args) throws Exception {
         SetZeroes code = new SetZeroes();
-        int[][] s = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        int[][] s = new int[][]{{0,1,2,0},{3,4,5,2},{1,3,1,5}};
         code.setZeroes(s);
     }
 
     public void setZeroes(int[][] matrix) {
-        byte[] row=new byte[25];
-        byte[] column=new byte[25];
         boolean flag=true;
+        boolean row=false,column=false;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
                 if (matrix[i][j] == 0) {
                     flag=false;
-                    row[i>>3]=(byte)(row[i>>3] | 1<<(i&7));
-                    column[j>>3]=(byte)(column[j>>3] | 1<<(j&7));
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                    if(i==0){
+                        row=true;
+                    }
+                    if(j==0){
+                        column=true;
+                    }
                 }
             }
         }
         if (flag) {
             return;
         }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if ((row[i>>3]&(1<<(i&7)))!=0) {
-                    matrix[i][j]=0;
-                }else if ((column[j>>3]&(1<<(j&7)))!=0) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                if (matrix[i][0]==0||matrix[0][j]==0) {
                     matrix[i][j]=0;
                 }
+            }
+        }
+        if(row){
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i]=0;
+            }
+        }
+        if(column){
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0]=0;
             }
         }
     }
