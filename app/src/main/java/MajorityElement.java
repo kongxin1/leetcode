f93ae405f20d248712dd3deb198ee46bbfe2c634
kongxin1 <kongxin1912@163.com>
2021-07-09 21:47:32 +0800

    面试题 17.10. 主要元素

--- app/src/main/java/MajorityElement.java
+++ app/src/main/java/MajorityElement.java
@@ -1,61 +1,36 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-/**
- * 面试题 17.10. 主要元素
- * 数组中占比超过一半的元素称之为主要元素。给定一个整数数组，找到它的主要元素。若没有，返回-1。
- * 执行结果：通过
- * 显示详情
- * 执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
- * 内存消耗：41.8 MB, 在所有 Java 提交中击败了44.59%的用户
- */
 public class MajorityElement {
-    public static void main(String[] args)throws Exception {
-        MajorityElement code=new MajorityElement();
-        Scanner sc = new Scanner(System.in);
-        while(true) {
-            String l = sc.nextLine();
-            String[] a=l.split(",");
-            int[] i=new int[a.length];
-            Integer[] in=Arrays.stream(a).map(x->Integer.valueOf(x)).toArray(Integer[]::new);
-
-            for(int t=0;t<i.length;t++){
-                i[t]=in[t];
-            }
-            System.out.println(code.majorityElement(i));
-        }
-    }
+	public static void main(String argv[]){
+		MajorityElement m=new MajorityElement();
+		m.majorityElement(new int[]{3,2});
+	}
     public int majorityElement(int[] nums) {
-        if(nums.length==0){
-            return -1;
-        }
-        if(nums.length==1){
-            return nums[0];
-        }
-        int res=nums[0];
-        int cnt=1;
-        for(int i=1;i< nums.length;i++){
-            if(res==nums[i]){
-                cnt++;
-            }else{
-                if(cnt==0){
-                    res=nums[i];
-                    cnt++;
-                }else{
-                    cnt--;
-                }
-            }
-        }
-        cnt=0;
-        for(int i=0;i< nums.length;i++){
-            if(res==nums[i]){
-                cnt++;
-            }
-        }
-        if(cnt>nums.length/2){
-            return res;
-        }else{
-            return -1;
-        }
+    	int cnt=1,e=nums[0];
+    	for(int i=1;i<nums.length;i++){
+    		if(cnt==0){
+    			e=nums[i];
+    			cnt=1;
+    			continue;
+    		}
+    		if(nums[i]==e){
+    			cnt++;
+    		}else{
+    			cnt--;
+    		}
+    	}
+    	if(cnt==0){
+    		return -1;
+    	}
+    	cnt=0;
+    	for(int i=0;i<nums.length;i++){
+    		if(e==nums[i]){
+    			cnt++;
+    		}
+    	}
+    	if(cnt>nums.length/2){
+    		return e;
+    	}else{
+    		return -1;
+    	}
     }
+
 }
