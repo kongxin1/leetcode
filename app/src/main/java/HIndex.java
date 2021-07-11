ecc7da0572b9263c25f1d6c8160f2b1d99a55380
kongxin1 <kongxin1912@163.com>
2021-07-11 13:23:14 +0800

    274. H 指数

--- app/src/main/java/HIndex.java
+++ app/src/main/java/HIndex.java
@@ -1,17 +1,31 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HIndex {
     public static void main(String argv[]) {
-        HIndex h=new HIndex();
-        System.out.print(h.hIndex(new int[]{3,0,6,1,5}));
+        HIndex h = new HIndex();
+        System.out.print(h.hIndex(new int[]{1, 1}));
     }
+
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        for(int i=0;i< citations.length;i++){
-            int c=citations.length-i;
-            if(citations[i]>=c){
-                return c;
+        int[] cnt = new int[citations.length + 1];
+
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length) {
+                cnt[citations.length]++;
+            } else {
+                cnt[citations[i]]++;
             }
+        }
+        int res = 0;
+        for (int i = citations.length; i >= 0; i--) {
+
+            res += cnt[i];
+            if (res >= i) {
+                return i;
+            }
+
         }
         return 0;
     }
