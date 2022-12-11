47ba56e4a364e39cbb63372a17549da9551e66a1
孔新 <kongxin1912@163.com>
2022-12-11 13:09:20 +0800

    189. 轮转数组

--- app/src/main/java/Rotate2.java
+++ app/src/main/java/Rotate2.java
@@ -5,20 +5,18 @@ public class Rotate2 {
     }
     public void rotate(int[] nums, int k) {
         k=k% nums.length;
-        int m=nums[0];
-        int n=nums[0];
+        int m,n;
         boolean flag[]=new boolean[nums.length];
-        for(int i=0;i< nums.length;i++){
+        for(int i=0;i< k;i++){
             if(flag[i]==false){
-                int j=i;
+                int j=(i+k)% nums.length;
                 m=nums[i];
-                while(flag[(j+k)% nums.length]==false){
-                    int next=(j+k)% nums.length;
-                    n=nums[next];
-                    nums[next]=m;
+                while(flag[j]==false){
+                    n=nums[j];
+                    nums[j]=m;
                     m=n;
-                    flag[next]=true;
-                    j=next;
+                    flag[j]=true;
+                    j=(j+k)% nums.length;
                 }
             }
         }
